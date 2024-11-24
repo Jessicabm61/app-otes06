2868e02f4800fbe119c5d572479c94a692f31341
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2024-11-24 19:07:38 -0300

    Update RetrofitClient.java

--- frontend/appotes06/app/src/main/java/api/RetrofitClient.java
+++ frontend/appotes06/app/src/main/java/api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.0.201:8080/";
+    private static final String BASE_URL = "http://localhost:8080/"; //Informar o IP e a porta
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
